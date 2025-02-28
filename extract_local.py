b22e8ca7cc369984f47202a3003d61577321dae7
Towphe <castillochristopherjames@gmail.com>
2025-02-28 22:59:08 +0800

    added code for going thru all regions except NCR

--- extract_local.py
+++ extract_local.py
@@ -223,7 +223,11 @@ def extract_politicians_in_province(link:str, region:str) -> pd.DataFrame|None:
             filename = f"{province_name}_{lgu_name}.pdf"
 
             # get file
-            driver.execute_script(f"arguments[0].setAttribute('download', '{filename}');", lgu_link)
+            try: 
+                driver.execute_script(f"arguments[0].setAttribute('download', '{filename}');", lgu_link)
+            except:
+                print(f"arguments[0].setAttribute('download', '{filename}');")
+                return
 
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((lgu_link)))
             # lgu_link.click()
@@ -269,6 +273,45 @@ def extract_politicians_in_province(link:str, region:str) -> pd.DataFrame|None:
 
     return None
 
-region_4A_link = "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R4A"
-region3_link = "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R3"
-extract_politicians_in_province(region3_link, "Region 3")
+regions = ({"region_name": "NCR", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_NCR"},
+    {"region_name": "CAR", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_CAR"},
+    {"region_name": "NIR", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_NIR"},
+    {"region_name": "I", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R1"},
+    {"region_name": "II", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R2"},
+    {"region_name": "III", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R3"},
+    {"region_name": "IV-A", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R4A"},
+    {"region_name": "IV-B", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R4B"},
+    {"region_name": "V", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R5"},
+    {"region_name": "VI", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R6"},
+    {"region_name": "VII", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R7"},
+    {"region_name": "VIII", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R8"},
+    {"region_name": "IX", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R9"},
+    {"region_name": "X", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R10"},
+    {"region_name": "XI", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R11"},
+    {"region_name": "XII", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R12"},
+    {"region_name": "CARAGA", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_R13"},
+    {"region_name": "BARMM", "link": "https://comelec.gov.ph/?r=2025NLE/CLC2025/CLC_BARMM"})
+
+# extract and load candidates from CAR, NIR and Region I
+# extract_politicians_in_province(regions[1]["link"], regions[1]["region_name"])
+# extract_politicians_in_province(regions[2]["link"], regions[2]["region_name"])
+# extract_politicians_in_province(regions[3]["link"], regions[3]["region_name"])
+
+# extract_politicians_in_province(regions[4]["link"], regions[4]["region_name"])
+# extract_politicians_in_province(regions[5]["link"], regions[5]["region_name"])
+# extract_politicians_in_province(regions[6]["link"], regions[6]["region_name"])
+
+# extract_politicians_in_province(regions[7]["link"], regions[7]["region_name"])
+# extract_politicians_in_province(regions[8]["link"], regions[8]["region_name"])
+# extract_politicians_in_province(regions[9]["link"], regions[9]["region_name"])
+
+# extract_politicians_in_province(regions[10]["link"], regions[10]["region_name"])
+# extract_politicians_in_province(regions[11]["link"], regions[11]["region_name"])
+# extract_politicians_in_province(regions[12]["link"], regions[12]["region_name"])
+
+# extract_politicians_in_province(regions[13]["link"], regions[13]["region_name"])
+# extract_politicians_in_province(regions[14]["link"], regions[14]["region_name"])
+# extract_politicians_in_province(regions[15]["link"], regions[15]["region_name"])
+
+# extract_politicians_in_province(regions[16]["link"], regions[16]["region_name"])
+# extract_politicians_in_province(regions[17]["link"], regions[17]["region_name"])
