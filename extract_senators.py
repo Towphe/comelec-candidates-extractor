a3768d42d90e7e936e1d4db4173f4d5714f0d5f5
Towphe <castillochristopherjames@gmail.com>
2025-02-28 18:21:06 +0800

    made senator extraction script save to db

--- extract_senators.py
+++ extract_senators.py
@@ -1,20 +1,44 @@
 import pdfplumber
 import pdfplumber.table
 import pandas as pd
+import psycopg
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+
+db_key = os.getenv("DB_KEY")
+
+print(db_key)
+
+def extract_senador(filename:str):
+    # initialize db connection
+    db = psycopg.connect(db_key, cursor_factory=psycopg.ClientCursor)
+
+    with pdfplumber.open(filename) as s_list:
+        table = pd.DataFrame()
+
+        for page in s_list.pages:
+            new_table = page.find_table().extract()
+
+            if new_table == None:
+                continue
+
+            table = pd.concat([table, pd.DataFrame(new_table)])
+        
+        # create cursor
+        cur = db.cursor()
+
+        # save data straight to db
+        with cur.copy("COPY senator (ballot_number, ballot_name, sex, name, partylist) FROM STDIN") as copy:
+            for candidate in table.values.tolist():
+                if re.match(r'^[0-9]+$', candidate[0]) != None:
+                    candidate[2] = candidate[2][0]
+                    copy.write_row(candidate)
+                
+        # save senator data
+        db.commit()
 
 filename = "CLC2025_Senator.pdf"
-
-with pdfplumber.open(filename) as s_list:
-    print(len(s_list.pages))
-
-    table = pd.DataFrame()
-
-    for page in s_list.pages:
-        new_table = page.find_table().extract()
-
-        if new_table == None:
-            continue
-
-        table = pd.concat([table, pd.DataFrame(new_table)])
-    
-    table.to_csv("senators.csv", index=False)
+extract_senador(filename)
